030bf5f0740d42239f6d826c371da088f2cef81e
PotatoLC <134663368+PotatoLC@users.noreply.github.com>
2023-08-03 02:05:23 -0600

    Update ApuntadorFunciones.c

--- Cap4/ApuntadorFunciones.c
+++ Cap4/ApuntadorFunciones.c
@@ -1,3 +1,9 @@
+/*
+El programa implementa un ordenador de líneas utilizando el método de ordenación QuickSort. Lee las líneas de entrada y las 
+almacena en un arreglo de punteros lineptr. Luego, ordena las líneas ya sea de forma numérica o alfabética según el argumento
+de línea de comando (-n para orden numérico). Finalmente, imprime las líneas ordenadas.
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include "librerialineas.h"
