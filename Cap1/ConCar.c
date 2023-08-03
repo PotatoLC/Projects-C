3a49384958cb2ac34c2bdc471f449fcc0312e5e6
PotatoLC <134663368+PotatoLC@users.noreply.github.com>
2023-08-03 01:53:02 -0600

    Update ConCar.c

--- Cap1/ConCar.c
+++ Cap1/ConCar.c
@@ -1,4 +1,8 @@
-
+/*
+El programa cuenta el número de líneas en la entrada de texto y muestra el resultado al final. 
+Utiliza un bucle while para leer cada carácter de la entrada hasta que se alcance el final del archivo (EOF), 
+incrementando el contador de líneas cada vez que encuentra un carácter de nueva línea '\n'
+*/
 #include <stdio.h>
 
 main(){
