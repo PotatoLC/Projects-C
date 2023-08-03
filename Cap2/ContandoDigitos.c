2947be722fd5debee738dc72c8961ab6dff95bf1
PotatoLC <134663368+PotatoLC@users.noreply.github.com>
2023-08-03 01:56:17 -0600

    Update ContandoDigitos.c

--- Cap2/ContandoDigitos.c
+++ Cap2/ContandoDigitos.c
@@ -1,3 +1,9 @@
+/*
+El programa cuenta y muestra el número de dígitos, espacios en blanco y caracteres que no son dígitos en la entrada de texto. 
+Utiliza un array ndigit para contar la frecuencia de cada dígito del 0 al 9. 
+Luego, muestra los conteos y el número de espacios en blanco y otros caracteres al final.
+*/
+
 
 #include <stdio.h>
 
